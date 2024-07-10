c78d2e7a23b6bfdd61eccab4d753ddb448e9989c
Leowod <leowod@hotmail.com>
2024-07-10 11:02:44 -0300

    Primeiro Commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Bem-vindo ao Portal do Funcionário</h1>
+      </header>
+    </div>
+  );
+}
